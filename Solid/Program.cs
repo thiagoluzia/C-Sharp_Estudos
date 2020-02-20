3f88ea298caf520b3a2c1c473d155fa08a458bd0
Thiago Moura <thiago.mouraluzia@gmail.com>
2020-02-20 00:13:07 -0300

    Princípio da Responsabilidade Única - Uma classe deve ter um, e somente um, motivo para mudar.

---
+++ Solid/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CSharpOO.SOLID.SRP.Solucao;
+
+
+
+namespace CSharpOO.SOLID
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            #region Thiago - SRP - Solução
+            Usuario user = new Usuario()
+            {
+                Codigo = 15,
+                CPF = "15778847854",
+                DataNascimento = new DateTime(1989, 08, 23),
+                Email = "thiago.mouraluzia@gmail.com",
+                Nome = "Thiago Moura"
+            };
+
+            UsuarioService usuarioService = new UsuarioService();
+            usuarioService.AdicionarUsuario(user);
+            #endregion
+
+            //CalculoArea.Calcular();
+
+            //CalculoAreaCorreto.Calcular();
+        }
+    }
+}
