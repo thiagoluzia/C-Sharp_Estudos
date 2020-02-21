97afa528ef1cc9ad6c5ed7888a95f7309a4888fd
Thiago Moura <thiago.mouraluzia@gmail.com>
2020-02-21 00:50:56 -0300

    Princípio do Aberto/Fechado -Uma classe deve estar aberta para extensão, mas deve estar fechada para modificação.

--- Solid/Program.cs
+++ Solid/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
+using CSharpOO.SOLID.OCP.Solucao;
 using CSharpOO.SOLID.SRP.Solucao;
-
-
 
 namespace CSharpOO.SOLID
 {
@@ -22,10 +21,33 @@ namespace CSharpOO.SOLID
             UsuarioService usuarioService = new UsuarioService();
             usuarioService.AdicionarUsuario(user);
             #endregion
+            
+            #region Thiago OCP Solução
+            Conta contaCorrente = new ContaCorrente()
+            {
+                Agencia = 0380,
+                NumeroConta = 0288345,
+                Saldo = 0
+            };
+            contaCorrente.Depositar(150m, "Thiago Moura");
+            contaCorrente.Sacar(100m, "Thiago Moura");
+
+
+            Conta contaPoupanca = new ContaPoupanca()
+            {
+                Agencia = 0010,
+                NumeroConta = 0000045,
+                Saldo = 0
+            };
+            contaPoupanca.Depositar(500m, "Alesandra Campos");
+            contaPoupanca.Sacar(250m, "Alesandra Campos");
+            #endregion
+
 
             //CalculoArea.Calcular();
 
             //CalculoAreaCorreto.Calcular();
+            Console.ReadKey();
         }
     }
 }
