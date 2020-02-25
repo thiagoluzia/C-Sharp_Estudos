d4537c538544fa24e10ee641ddcd47a6afacc042
Thiago Moura <thiago.mouraluzia@gmail.com>
2020-02-25 16:46:39 -0300

    Princípio da Segregação de Interface - Clientes não devem ser forçados a depender de métodos que não usam.

---
+++ Solid/4 - ISP/ISP.Solucao/Interfaces/ICadastroProduto.cs
@@ -0,0 +1,8 @@
+//ISP = Princípio da Segregação de Interface
+namespace CSharpOO.SOLID.ISP.Solucao.Interfaces
+{
+    public interface ICadastroProduto : ICadastro
+    {
+        //Aqui eu poderia ter algo direcionado ao cadastro do produto
+    }
+}
