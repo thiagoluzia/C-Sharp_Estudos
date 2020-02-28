cd271bddb495cc74aa7b0a2e5cad62e29067516b
Thiago Moura <thiago.mouraluzia@gmail.com>
2020-02-28 19:47:27 -0300

    Leitura completa de um arquivo de texto

--- ManipulacaoDeArquivos/ProjetoSimples/Program.cs
+++ ManipulacaoDeArquivos/ProjetoSimples/Program.cs
@@ -24,15 +24,32 @@ namespace ProjetoSimples
             //  Escrever dados em um arquivo!
             //-----------------------------------
 
+            /*
             //1º Criar um canal de escrita
             StreamWriter escritor = new StreamWriter(caminho, append: true);//4º passo ou poderia ter sido o 1º é inserir append: true apos o caminho, pois a cada chamada do metodo ele iria apagar o arquivo e reescrever,com o append ele sempre vai adicionar uma nova linha ao final do arquivo
-            //2ºChamar o metodo de escrita
+            //2º Chamar o metodo de escrita
             escritor.Write("Thiago Moura - sem quebra de linha");
             escritor.WriteLine("Thiago Henrique Moura Luzia - com quebra de linha 1");
             escritor.WriteLine("com quebra de linha 2");
             escritor.WriteLine("com quebra de linha 3");
             //3ºFechando o canal de escrita
             escritor.Close(); 
+            */
+
+            //-----------------------------------
+            //  Ler dados de um arquivo de texto!
+            //-----------------------------------
+
+            //1º Criar um canal de leitura com o arquivo
+            StreamReader leitor = new StreamReader(caminho);
+            //2º Criar uma variavel para armazenar o dado lido por metodo de leitura ReadToEnd, vai ler completamente todo conteudo do arquivo" 
+            string foiescrito2 = leitor.ReadToEnd();
+            //3º Fechando o canal de leitura
+            leitor.Close();
+            Console.WriteLine(foiescrito2);
+
+            Console.ReadKey();
+
 
         }
     }
