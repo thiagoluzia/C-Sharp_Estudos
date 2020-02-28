4001b27486d05f019a0553bd25db8bf11c3989bd
Thiago Moura <thiago.mouraluzia@gmail.com>
2020-02-28 19:31:33 -0300

    Escrita de dados em um arquivo de texto

--- ManipulacaoDeArquivos/ProjetoSimples/Program.cs
+++ ManipulacaoDeArquivos/ProjetoSimples/Program.cs
@@ -19,6 +19,21 @@ namespace ProjetoSimples
             {
                 File.Create(caminho);
             }
+
+            //-----------------------------------
+            //  Escrever dados em um arquivo!
+            //-----------------------------------
+
+            //1º Criar um canal de escrita
+            StreamWriter escritor = new StreamWriter(caminho, append: true);//4º passo ou poderia ter sido o 1º é inserir append: true apos o caminho, pois a cada chamada do metodo ele iria apagar o arquivo e reescrever,com o append ele sempre vai adicionar uma nova linha ao final do arquivo
+            //2ºChamar o metodo de escrita
+            escritor.Write("Thiago Moura - sem quebra de linha");
+            escritor.WriteLine("Thiago Henrique Moura Luzia - com quebra de linha 1");
+            escritor.WriteLine("com quebra de linha 2");
+            escritor.WriteLine("com quebra de linha 3");
+            //3ºFechando o canal de escrita
+            escritor.Close(); 
+
         }
     }
 }
