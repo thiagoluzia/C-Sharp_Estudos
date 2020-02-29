287d4379dcbbe246f81ce819c1f95861271ffd12
Thiago Moura <thiago.mouraluzia@gmail.com>
2020-02-28 21:38:49 -0300

    Leitura de um arquivo de texto - linha a linha

--- ManipulacaoDeArquivos/ProjetoSimples/Program.cs
+++ ManipulacaoDeArquivos/ProjetoSimples/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;//1º Importar  namespace para trabalhar com arquivos
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace ProjetoSimples
 {
@@ -11,14 +12,16 @@ namespace ProjetoSimples
             //  Criação de um arquivo!
             //-----------------------------------
 
+            
             //2º Criar uma string com o caminho para o arquivo, contendo seu nome e sua extensão
             string caminho = "./nomeDoArquivo.son";
-
+            /*
             //3º Verificar se o arquivo já existe no caminho passado, caso não exista o metodo create irá cria-lo
             if (!File.Exists(caminho))
             {
                 File.Create(caminho);
             }
+            */
 
             //-----------------------------------
             //  Escrever dados em um arquivo!
@@ -39,7 +42,7 @@ namespace ProjetoSimples
             //-----------------------------------
             //  Ler dados de um arquivo de texto!
             //-----------------------------------
-
+            /*
             //1º Criar um canal de leitura com o arquivo
             StreamReader leitor = new StreamReader(caminho);
             //2º Criar uma variavel para armazenar o dado lido por metodo de leitura ReadToEnd, vai ler completamente todo conteudo do arquivo" 
@@ -47,7 +50,29 @@ namespace ProjetoSimples
             //3º Fechando o canal de leitura
             leitor.Close();
             Console.WriteLine(foiescrito2);
+            */
 
+
+            //---------------------------------------------------
+            //  Ler dados de um arquivo de texto - Linha a Linha!
+            //---------------------------------------------------
+            
+            //1º Criar um canal de Leitura
+            StreamReader leitor = new StreamReader(caminho);
+            //2º Criar um laço para varrer o leitor (Enquanto leitor não chegar ao final do arquivo)
+            while (!leitor.EndOfStream)
+            {
+                //Processo ReadLine
+                //1 - Ler a linha
+                //2 - Retornar os dados da linha
+                //3 - Passa para a proxima linha
+                Console.WriteLine(leitor.ReadLine());
+            }
+            //3ºFechar o canal de comunicação 
+            leitor.Close();
+            
+
+            
             Console.ReadKey();
 
 
