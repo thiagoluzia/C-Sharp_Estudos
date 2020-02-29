518724ccaa65e69f656c5f841b8171f4ec53b935
Thiago Moura <thiago.mouraluzia@gmail.com>
2020-02-29 19:31:33 -0300

    Expressoes Lambda, funcoes que nao tem nome nem modificador de acesso

---
+++ ExpressoesLambda/ExpressoesLambda/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExpressoesLambda
+{
+    class Program
+    {
+        //Criando um delegate para nao utilizar o generico Func<> oferecido pela linguagem
+        delegate int semparametro(int numero);
+      
+        static void Main(string[] args)
+        {
+            //Parametros => Implementação 
+            Func<int, int> quadrado = x => x * x;
+            Console.WriteLine(quadrado(10));
+
+            //Com meu delegate
+            semparametro meudelegate = x => x * x;
+            Console.WriteLine(meudelegate(5));
+
+            Console.ReadKey();
+        }
+    }
+}
