4a635cda86a59d595883c4be6f2636f9731e937e
Thiago Moura <thiago.mouraluzia@gmail.com>
2020-02-29 23:07:16 -0300

    Extension methods, Extendendo a funcionalidade de uma classe sem ter que alterar seu codigo fonte

---
+++ ExtensionMethods/ExtensionMethods/ExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethods
+{
+    static class Strings
+    {
+        public static string SchoolOfNet(this string texto)
+        {
+            return texto + " - School of net";
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ExtensionMethods;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Thiago Moura";
+
+            Console.WriteLine(nome.SchoolOfNet());
+
+            Console.ReadKey();
+
+        }
+    }
+}
